46f9faa06584d116c11ace7ef49124eb2703e4c7
khaled <khaled.ahmed.ext@orange.com>
2026-04-15 15:29:27 +0200

    solve first problem

---
+++ problem-001/src/Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+    int[] nums1 = {1, 2, 3, 0, 0, 0};
+    int m = 3;
+    int[] nums2 = {2, 5, 6};
+    int n = 3;
+    merge(nums1, m, nums2, n);
+}
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        for (int i = 0; i < n; i++) {
+            nums1[m + i] = nums2[i];
+        }
+        Arrays.sort(nums1);
+    }
